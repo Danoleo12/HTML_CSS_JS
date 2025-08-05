d578db1461ade9d05ffb4b19609368ed592e219c
Danick Ditchou <dn303068@dal.ca>
2025-08-05 15:02:00 -0300

    A lot of improvement on the UI. Contact information added, Forms hidden and displayed when buttons are clicked

--- Dal_Course_Feedback/Scripts/scripts.js
+++ Dal_Course_Feedback/Scripts/scripts.js
@@ -1,6 +1,10 @@
 let subject = document.getElementById("subject").value;
 console.log("subject: " + subject);
 
-function showContent() {
+function showCreateComment() {
     document.getElementById("create-comment").style.display = "block"; //Display the form to make a new comment only when the "create comment button is clicked"
+}
+
+function showSearchReview() {
+    document.getElementById("searchReview").style.display = "block"; //Display the form to search reveiew only when the " button is clicked"
 }
