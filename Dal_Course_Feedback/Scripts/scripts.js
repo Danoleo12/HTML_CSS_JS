1b89fa4341521f1be3313be841d25faf37bf62a8
Danick Ditchou <dn303068@dal.ca>
2025-08-04 23:54:13 -0300

    Satrted working on Dal review

---
+++ Dal_Course_Feedback/Scripts/scripts.js
@@ -0,0 +1,6 @@
+let subject = document.getElementById("subject").value;
+console.log("subject: " + subject);
+
+function showContent() {
+    document.getElementById("create-comment").style.display = "block"; //Display the form to make a new comment only when the "create comment button is clicked"
+}
